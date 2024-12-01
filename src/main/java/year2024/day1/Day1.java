42a8f1fb67ec54e3fd9bde6933dfde60efd7fb91
Tim Seiler <tim.seiler@students.uni-mannheim.de>
2024-12-01 16:34:38 +0100

    fixed merge sort error, added solution to part 2, comments

--- src/main/java/year2024/day1/Day1.java
+++ src/main/java/year2024/day1/Day1.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Part1 {
+public class Day1 {
 
   static List<Integer> list1 = new ArrayList<>();
   static List<Integer> list2 = new ArrayList<>();
@@ -16,15 +16,18 @@ public class Part1 {
     list1 = mergeSort(list1);
     list2 = mergeSort(list2);
     System.out.println("Total distance = " + getTotalDistance(list1, list2));
+    System.out.println("Similarity score = " + getSimilarity(list1, list2));
   }
 
+  /**
+   * Splits input file into two lists of integers
+   *
+   * @param file filepath
+   */
   private static void splitInput(String file) {
-    FileReader fr = null;
-    BufferedReader br = null;
-
     try {
-      fr = new FileReader(file);
-      br = new BufferedReader(fr);
+      FileReader fr = new FileReader(file);
+      BufferedReader br = new BufferedReader(fr);
 
       String line = br.readLine();
       while (line != null) {
@@ -37,16 +40,29 @@ public class Part1 {
     }
   }
 
+  /**
+   * Sorts a list of integers using merge sort
+   *
+   * @param list list to sort
+   * @return sorted list
+   */
   private static List<Integer> mergeSort(List<Integer> list) {
     if (list.size() <= 1) {
       return list;
     } else {
-      List<Integer> left = new ArrayList<>(list.subList(0, list.size() / 2));
-      List<Integer> right = new ArrayList<>(list.subList(list.size() / 2, list.size()));
+      List<Integer> left = mergeSort(new ArrayList<>(list.subList(0, list.size() / 2)));
+      List<Integer> right = mergeSort(new ArrayList<>(list.subList(list.size() / 2, list.size())));
       return merge(left, right);
     }
   }
 
+  /**
+   * Merges two lists following merge sort procedure
+   *
+   * @param left  list1
+   * @param right list2
+   * @return merged list
+   */
   private static List<Integer> merge(List<Integer> left, List<Integer> right) {
     List<Integer> merged = new ArrayList<>();
     while (!left.isEmpty() && !right.isEmpty()) {
@@ -69,11 +85,42 @@ public class Part1 {
     return merged;
   }
 
+  /**
+   * Calculates the total sum of the difference of two values at the same index
+   *
+   * @param list1 first list
+   * @param list2 second list
+   * @return total distance of two lists
+   */
   private static int getTotalDistance(List<Integer> list1, List<Integer> list2) {
     int totalDistance = 0;
     for (int i = 0; i < list1.size(); i++) {
       totalDistance += Math.abs(list1.get(i) - list2.get(i));
     }
     return totalDistance;
+  }
+
+  /**
+   * Calculates the sum for all values in list 1: value * occurrences in list2
+   *
+   * @param list1 first list
+   * @param list2 second list
+   * @return similarity score
+   */
+  private static int getSimilarity(List<Integer> list1, List<Integer> list2) {
+    int similarity = 0;
+    for (int i = 0; i < list1.size(); i++) {
+      for (int j = 0; j < list2.size(); j++) {
+        if (list1.get(i).equals(list2.get(j))) {
+          int count = 1;
+          while (list1.get(i).equals(list2.get(++j))) {
+            count++;
+          }
+          similarity += list1.get(i) * count;
+          break;
+        }
+      }
+    }
+    return similarity;
   }
 }
